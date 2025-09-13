e06ab832a8687c29bff430cf1fd731a8fe8b3b47
Ezik143 <jakesucgang90@gmail.com>
2025-09-13 18:46:39 +0800

    it will never enough

---
+++ Codes/DSA/SingleLinkedList.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char *name;
+    int age;
+    char *course;
+    int year;
+    Student * next;
+}Student;
+
+
+struct Node * createNode(int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(Node));
+}
+//IT WILL NEVER ENOUGH
+
+int main()
+{
+
+}
