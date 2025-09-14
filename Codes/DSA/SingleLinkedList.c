5a71f5d5c3d6ff8a2ab8d9150ef10e26370c9dcf
Ezik143 <jakesucgang90@gmail.com>
2025-09-15 05:08:56 +0800

    hayst

--- Codes/DSA/SingleLinkedList.c
+++ Codes/DSA/SingleLinkedList.c
@@ -1,21 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+struct Student
+{
+    char *Name;
+    int Age;
+    char *Course;
+    int Year;
+    struct Student *next;
+};
 
-typedef struct{
-    char *name;
-    int age;
-    char *course;
-    int year;
-    Student * next;
-}Student;
-
-
-struct Node * createNode(int data){
-    struct Node* newNode = (struct Node*)malloc(sizeof(Node));
+struct Student *head;
+void InsertAtBeginning(char *name, int age, char *course, int year)
+{
+    struct Student *newNode = (struct Student *)malloc(sizeof(struct Student));
+    newNode->Name = strdup(name);
+    newNode->Age = age;
+    newNode->Course = strdup(course);
+    newNode->Year = year;
+    newNode->next = NULL;
+    if (head != NULL)
+    {
+        newNode->next = head;
+    }
+    head = newNode;
 }
-//IT WILL NEVER ENOUGH
+
+void InsertAtEnd(char *name, int age, char *course, int year)
+{
+    struct Student *newNode = (struct Student *)malloc(sizeof(struct Student));
+    newNode->Name = strdup(name);
+    newNode->Age = age;
+    newNode->Course = strdup(course);
+    newNode->Year = year;
+    newNode->next = NULL;
+    struct Student *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void InsertAtPosition(char *name, int age, char *course, int year, int position)
+{
+    struct Student *newNode = (struct Student *)malloc(sizeof(struct Student));
+    newNode->Name = strdup(name);
+    newNode->Age = age;
+    newNode->Course = strdup(course);
+    newNode->Year = year;
+    newNode->next = NULL;
+    int index = 1;
+    struct Student *temp = head;
+    //index - 1 so that 
+    while (temp != NULL && index < position - 1)
+    {
+        temp = temp->next;
+        index++;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void Print()
+{
+    struct Student *temp = head;
+    while (temp != NULL)
+    {
+        printf("Name: %s, Age: %d, Course: %s, Year: %d -> ", temp->Name, temp->Age, temp->Course, temp->Year);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+// IT WILL NEVER ENOUGH
 
 int main()
 {
-
+    InsertAtBeginning("jake", 20, "BSIT", 2);
+    InsertAtBeginning("tonio", 20, "BSIT", 3);
+    InsertAtEnd("zeke", 20, "BSIT", 2);
+    InsertAtPosition("ezik", 20, "BSIT", 2, 2);
+    Print();
+    return 0;
 }
